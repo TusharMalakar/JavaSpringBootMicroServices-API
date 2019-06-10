94fac358f813b6be285fb3708a2b5fa88f6ea3be
Tushar Malakar <35859780+TusharMalakar@users.noreply.github.com>
2019-06-09 22:22:24 -0400

    adding spring security

---
+++ src/main/java/com/JavaAPI_SpringBoot/Home/JWTsecurity/jsonSecurity.java
@@ -0,0 +1,14 @@
+package com.JavaAPI_SpringBoot.Home.JWTsecurity;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class jsonSecurity extends WebSecurityConfigurerAdapter {
+
+	//JWT session token
+}
