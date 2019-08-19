8f94505bec06254c83ce63c572ba4cbd6cb9a0c1
Tushar Malakar <35859780+TusharMalakar@users.noreply.github.com>
2019-08-19 17:09:11 -0400

    SHA-512

--- src/main/java/com/JavaAPI_SpringBoot/Home/security/EncryptionDecryption.java
+++ src/main/java/com/JavaAPI_SpringBoot/Home/security/EncryptionDecryption.java
@@ -21,7 +21,7 @@ public class EncryptionDecryption {
         MessageDigest sha = null;
         try {
             key = myKey.getBytes("UTF-8");
-            sha = MessageDigest.getInstance("SHA-1");
+            sha = MessageDigest.getInstance("SHA-512");
             key = sha.digest(key);
             key = Arrays.copyOf(key, 16);
             secretKey = new SecretKeySpec(key, "AES");
